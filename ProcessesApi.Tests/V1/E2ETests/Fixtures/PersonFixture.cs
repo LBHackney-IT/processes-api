20362b8650ff3bed6584967e8ed0d4e1656a6f70
AtiyyahRawat <atiyyah.rawat@gmail.com>
2022-03-09 16:10:07 +0000

    Update E2E tests

--- ProcessesApi.Tests/V1/E2ETests/Fixtures/PersonFixture.cs
+++ ProcessesApi.Tests/V1/E2ETests/Fixtures/PersonFixture.cs
@@ -8,6 +8,8 @@ using Hackney.Shared.Person.Factories;
 using Hackney.Shared.Person.Infrastructure;
 using ProcessesApi.V1.Factories;
 using Amazon.DynamoDBv2.DataModel;
+using System.Linq;
+using Hackney.Shared.Tenure.Domain;
 
 namespace ProcessesApi.Tests.V1.E2E.Fixtures
 {
@@ -55,6 +57,19 @@ namespace ProcessesApi.Tests.V1.E2E.Fixtures
 
         public void GivenAPersonDoesNotExist()
         {
+        }
+
+        public async Task GivenAPersonHasAnActiveTenure(Guid tenureId)
+        {
+            var tenures = Person.Tenures.Append(_fixture.Build<TenureDetails>()
+                                                        .With(x => x.Id, tenureId)
+                                                        .With(x => x.EndDate, DateTime.UtcNow.AddYears(10).ToString())
+                                                        .With(x => x.Type, TenureTypes.Secure.Code)
+                                                        .Create());
+            Person.Tenures = tenures;
+            Person.VersionNumber = 0;
+
+            await _dbContext.SaveAsync<PersonDbEntity>(Person.ToDatabase()).ConfigureAwait(false);
         }
 
     }
