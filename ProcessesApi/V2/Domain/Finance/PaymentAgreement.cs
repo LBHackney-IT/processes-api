25ae27396a7d9ab23b793cb10d495535f08eeb1f
Ray Mitchell <ray@Rays-Air-2.broadband>
2022-08-25 11:53:51 +0100

    mttl-3000-refactor

--- ProcessesApi/V2/Domain/Finance/PaymentAgreement.cs
+++
@@ -1,8 +0,0 @@
-// namespace ProcessesApi.V2.Domain.Finance
-// {
-//     public class PaymentAgreement
-//     {
-//         public string TenancyRef { get; set; }
-//         public float? Amount { get; set; }
-//     }
-// }
